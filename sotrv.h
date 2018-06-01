a3d74b71454970a7fa878d9f6787974984d415d8
Winchv <32427417+Winchv@users.noreply.github.com>
2018-06-01 11:54:45 +0300

    Create sotrv.h

---
+++ sotrv.h
@@ -0,0 +1,17 @@
+# define MAX 100000 
+
+void FillMostly(float *A, int N); 
+void FillEq(float *A, int N); 
+void FillDecr(float *A, int N); 
+void FillIncr(float *A, int N); 
+void FillRand(float *A, int N); 
+void BubbleSort1(float *A, int N); 
+void InsertionSort(float *A, int N); 
+void SelectionSort(float *A, int N); 
+void HeapCorrection(float *A, int N, int i); 
+void HeapSort(float *A, int N); 
+void MergeSort(float *A, int N); 
+void QuickSort(float *A, int N); 
+void Output(float *A, int N); 
+void Swap(float *A, float *B); 
+int CheckSort(float *A, int N);
